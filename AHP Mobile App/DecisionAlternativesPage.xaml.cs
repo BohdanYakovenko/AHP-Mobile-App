007ef13360b37d802bf9806e5ae9d13c46e81e4b
BohdanYakovenko <byakovenko@kse.org.ua>
2024-05-08 16:07:32 +0100

    draft of DecionAlternatives page logic: step 8

--- AHP Mobile App/DecisionAlternativesPage.xaml.cs
+++ AHP Mobile App/DecisionAlternativesPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +12,95 @@ namespace AHP_Mobile_App
         public DecisionAlternativesPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LoadDecisionAlternatives();
+        }
+
+        private void LoadDecisionAlternatives()
+        {
+            var hierarchyData = App.HierarchyData;
+            var readyToDisplay = new List<object>();
+            HashSet<string> missingDataParents = new HashSet<string>();
+
+            for (int i = 0; i < hierarchyData.Count; i++)
+            {
+                Node node = hierarchyData[i];
+                double globalPriority = double.NaN; // Assume we can't calculate it by default
+
+                if (node.Children != null && node.Children.Count > 0)
+                {
+                    if (node.LocalPriorities == null || node.LocalPriorities.Count == 0)
+                    {
+                        missingDataParents.Add(node.Name);
+                    }
+                    else
+                    {
+                        globalPriority = CalculateGlobalPriority(node, missingDataParents);
+                    }
+                }
+
+                // Always add node to display; show global priority if calculable
+                string priorityDisplay = double.IsNaN(globalPriority) ? "N/A" : $"{globalPriority:P2}";
+                readyToDisplay.Add(new { Name = node.Name, Priority = priorityDisplay });
+            }
+
+            ListView1.ItemsSource = readyToDisplay;
+
+            if (missingDataParents.Count > 0)
+            {
+                SetInfoLabel(missingDataParents);
+            }
+        }
+
+        private double CalculateGlobalPriority(Node node, HashSet<string> missingDataParents)
+        {
+            double globalPriority = 1.0;
+            Node currentNode = node;
+            bool missingData = false;
+
+            while (currentNode != null)
+            {
+                Node parentNode = null;
+                foreach (var potentialParent in App.HierarchyData)
+                {
+                    if (potentialParent.Children != null && potentialParent.Children.Contains(currentNode.Name))
+                    {
+                        parentNode = potentialParent;
+                        break;
+                    }
+                }
+
+                if (parentNode == null || parentNode == App.HierarchyData.First()) break;
+
+                if (parentNode.LocalPriorities == null || parentNode.LocalPriorities.Count == 0)
+                {
+                    missingDataParents.Add(parentNode.Name);
+                    missingData = true;
+                    break; // Do not continue calculating if data is missing
+                }
+
+                if (!missingData)
+                {
+                    int index = parentNode.Children.IndexOf(currentNode.Name);
+                    globalPriority *= parentNode.LocalPriorities[index];
+                }
+
+                currentNode = parentNode;
+            }
+
+            return missingData ? double.NaN : globalPriority;
+        }
+
+        private void SetInfoLabel(HashSet<string> missingDataParents)
+        {
+            string firstMissing = missingDataParents.First();
+            int additionalMissingCount = missingDataParents.Count - 1;
+            InfoLabel.Text = $"Ratings of the alternatives are not available for '{firstMissing}' and {additionalMissingCount} more nodes require evaluation.";
+            InfoLabel.IsVisible = true;
         }
     }
 }
